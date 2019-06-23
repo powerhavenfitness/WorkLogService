ae26a59786a8f101b4510fc8b928b7d747b70e9f
gusto132 <gusto132@gmail.com>
2019-06-23 15:31:04 +0800

    changes

---
+++ WorkLogService.Core/Models/Defintion.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkLogService.Core.Models
+{
+    public class Defintion : BaseModel
+    {
+        public int VariableId { get; set; }
+        public string Value { get; set; }
+    }
+}
